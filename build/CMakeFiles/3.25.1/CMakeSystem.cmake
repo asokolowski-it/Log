97dc799092bba989e23ff1e769a5aca4d22ed9fc
Aleksy Sokolowski <aleksy.sokolowski.91@gmail.com>
2024-06-15 12:17:56 +0200

    adjustment

--- build/CMakeFiles/3.25.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.25.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.1.0-21-amd64")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-
+include("/home/as/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Linux-6.1.0-21-amd64")
 set(CMAKE_SYSTEM_NAME "Linux")
