e19d77702c8781aeeec5346143299c5bdf0e7b87
Aleksy Sokolowski <aleksy.sokolowski.91@gmail.com>
2024-06-26 22:30:25 +0200

    normal init

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include <pcl/common/centroid.h>
 #include <pcl/common/pca.h>
 #include <pcl/common/transforms.h>
-#include <cmath>
+#include <pcl/features/normal_3d.h>
 
+// Laden der Punktwolke
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr loadPointCloud(const std::string& filename) {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     if (pcl::io::loadPLYFile<pcl::PointXYZRGB>(filename, *cloud) == -1) {
@@ -16,32 +17,51 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr loadPointCloud(const std::string& filenam
     return cloud;
 }
 
-void findAndAlignCentralAxis(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, float x_adjustment = 0.0f, float y_adjustment = 0.0f, float z_adjustment = 0.0f) {
-    // Calculate the centroid of the entire point cloud
+// Berechnung und Hinzufügung der Normalen zur Punktwolke
+pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr calculateNormals(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+    pcl::copyPointCloud(*cloud, *cloud_with_normals);
+
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
+    ne.setInputCloud(cloud);
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>());
+    ne.setSearchMethod(tree);
+
+    pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+    ne.setRadiusSearch(0.03);
+    ne.compute(*normals);
+
+    for (size_t i = 0; i < cloud_with_normals->points.size(); ++i) {
+        cloud_with_normals->points[i].normal_x = normals->points[i].normal_x;
+        cloud_with_normals->points[i].normal_y = normals->points[i].normal_y;
+        cloud_with_normals->points[i].normal_z = normals->points[i].normal_z;
+    }
+
+    return cloud_with_normals;
+}
+
+// Zentrieren und Ausrichten der Punktwolke
+void findAndAlignCentralAxis(pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr& cloud, float x_adjustment = 0.0f, float y_adjustment = 0.0f, float z_adjustment = 0.0f) {
     Eigen::Vector4f centroid;
     pcl::compute3DCentroid(*cloud, centroid);
 
-    // Translate the point cloud to center it along the x-axis
     Eigen::Matrix4f translation = Eigen::Matrix4f::Identity();
     translation(0, 3) = -centroid[0] + x_adjustment;
     translation(1, 3) = -centroid[1] + y_adjustment;
     translation(2, 3) = -centroid[2] + z_adjustment;
-    pcl::transformPointCloud(*cloud, *cloud, translation);
+    pcl::transformPointCloudWithNormals(*cloud, *cloud, translation);
 
-    // Perform PCA to align the principal component with the x-axis
-    pcl::PCA<pcl::PointXYZRGB> pca;
+    pcl::PCA<pcl::PointXYZRGBNormal> pca;
     pca.setInputCloud(cloud);
     Eigen::Matrix3f eigenVectors = pca.getEigenVectors();
 
-    // Align the principal axis with the x-axis
     Eigen::Matrix4f transform = Eigen::Matrix4f::Identity();
     transform.block<3, 3>(0, 0) = eigenVectors.transpose();
-
-    // Transform the point cloud to align it
-    pcl::transformPointCloud(*cloud, *cloud, transform);
+    pcl::transformPointCloudWithNormals(*cloud, *cloud, transform);
 }
 
-float calculateBiggestRadius(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
+// Berechnung des größten Radius
+float calculateBiggestRadius(pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr& cloud) {
     float max_radius = 0.0;
     for (const auto& point : cloud->points) {
         float radius = std::sqrt(point.y * point.y + point.z * point.z);
@@ -52,30 +72,31 @@ float calculateBiggestRadius(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
     return max_radius;
 }
 
-void transformPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, float biggest_radius) {
+// Transformation der Punktwolke
+void transformPointCloud(pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr& cloud, float biggest_radius) {
     for (auto& point : cloud->points) {
         float radius = std::sqrt(point.y * point.y + point.z * point.z);
         float theta = std::atan2(point.y, point.z); // Unwrapping around z-axis
 
-        // Adjust theta to start unwrapping from the plus z-axis
         if (theta < 0) {
             theta += 2 * M_PI;
         }
 
-        // Map theta to a linear coordinate (unwrap) starting from the point on the plus z-axis that is equal to the biggest radius
         point.y = theta * biggest_radius;
         point.z = radius - biggest_radius;
     }
 }
 
-bool savePointCloud(const std::string& filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
-    if (pcl::io::savePLYFile<pcl::PointXYZRGB>(filename, *cloud) == -1) {
+// Speichern der Punktwolke
+bool savePointCloud(const std::string& filename, pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr& cloud) {
+    if (pcl::io::savePLYFile<pcl::PointXYZRGBNormal>(filename, *cloud) == -1) {
         PCL_ERROR("Couldn't write file %s \n", filename.c_str());
         return false;
     }
     return true;
 }
 
+// Hauptfunktion
 int main(int argc, char** argv) {
     if (argc != 8) {
         std::cerr << "Usage: " << argv[0] << " <input_ply_file> <aligned_ply_file> <output_ply_file> <x_adjustment> <y_adjustment> <z_adjustment> <save_aligned>" << std::endl;
@@ -95,19 +116,20 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    findAndAlignCentralAxis(cloud, x_adjustment, y_adjustment, z_adjustment);
+    auto cloud_with_normals = calculateNormals(cloud);
+
+    findAndAlignCentralAxis(cloud_with_normals, x_adjustment, y_adjustment, z_adjustment);
     
-    // Save the point cloud after aligning the central axis if the flag is true
     if (save_aligned) {
-        if (!savePointCloud(aligned_filename, cloud)) {
+        if (!savePointCloud(aligned_filename, cloud_with_normals)) {
             return -1;
         }
     }
 
-    float biggest_radius = calculateBiggestRadius(cloud);
-    transformPointCloud(cloud, biggest_radius);
+    float biggest_radius = calculateBiggestRadius(cloud_with_normals);
+    transformPointCloud(cloud_with_normals, biggest_radius);
 
-    if (!savePointCloud(output_filename, cloud)) {
+    if (!savePointCloud(output_filename, cloud_with_normals)) {
         return -1;
     }
 
